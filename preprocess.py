deb929dbf72c5da111cf989d14188f6bbb637b27
RA2111027010125 <103249371+RA2111027010125@users.noreply.github.com>
2025-03-31 13:35:05 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -120,4 +120,4 @@ def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"
 
 
 if __name__ == "__main__":
-    process_posts("C:/LinkedIn/data/raw_posts.json", "C:/LinkedIn/data/processed_posts.json")
+    process_posts("data/raw_posts.json", "data/processed_posts.json")
